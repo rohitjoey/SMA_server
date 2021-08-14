01e94ce178d470bfd46db9db15a1c14aaa9699a9
rohitjoey <roht.praz@gmail.com>
2021-08-14 22:55:28 +0545

    Config error

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://nirjal123:nirjal123@cluster0.6jptv.mongodb.net/SocialMedia?retryWrites=true&w=majority",
+
+  SECRET_KEY: "my secret key is i am batman",
+};
